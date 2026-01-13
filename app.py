f7e7e4884ab688eaffd7a5f5c55274aabe71b4a8
SPN <75294406+RPSPN@users.noreply.github.com>
2026-01-13 16:01:05 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ MOT_DE_PASSE_MAITRE = "admin123"  # <--- CHANGEZ LE MOT DE PASSE ICI
 
 LISTE_ITEMS_PREDEFINIS = [
     "Lait", "Oranges", "Huile", "Café", "Pâtes", 
-    "Riz", "Conserves", "Pain", "Oeufs", "Beurre", 
-    "Légumes frais", "Viande", "Savon", "Dentifrice",
-    "Couches", "Céréales", "Jus"
+    "Riz", "Conserves", "Pain", "Oeufs", "Oignons", 
+    "Carottes", "Poulet", "Boeuf", "Lentilles",
+    "Chips", "Céréales", "Extra"
 ]
 
 # Initialisation de l'état de l'application (Session State)
